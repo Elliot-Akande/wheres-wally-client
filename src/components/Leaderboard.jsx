4d61003902b5169590c623d74c48816f771d0487
Elliot Akande <elbo.lynch@gmail.com>
2024-01-16 12:38:28 +0000

    Create Leaderboard component

---
+++ src/components/Leaderboard.jsx
@@ -0,0 +1,39 @@
+import useLeaderboardData from "../hooks/useLeaderboardData";
+import PropTypes from "prop-types";
+
+function Leaderboard({ levelNum }) {
+  const { data, loading, error } = useLeaderboardData(levelNum);
+
+  const formatTime = (time) => {
+    const mins = Math.floor(time / 60);
+    const secs = time % 60;
+    const string = `${mins}:${secs}`;
+
+    return string.padStart(5, 0);
+  };
+
+  return (
+    <div>
+      <h2>Leaderboard</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Data fetching error.</p>
+      ) : (
+        <ul>
+          {data.map((entry) => (
+            <li key={entry.id}>
+              {entry.name} - {formatTime(entry.time)}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+Leaderboard.propTypes = {
+  levelNum: PropTypes.number,
+};
+
+export default Leaderboard;
