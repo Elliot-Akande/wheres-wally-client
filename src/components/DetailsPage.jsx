3ef18355167b57f0a45b1b41381bfc69acdc3d7c
Elliot Akande <elbo.lynch@gmail.com>
2024-01-16 12:38:51 +0000

    Add title, links and leaderboard

--- src/components/DetailsPage.jsx
+++ src/components/DetailsPage.jsx
@@ -1,5 +1,17 @@
+import { Link, useParams } from "react-router-dom";
+import Leaderboard from "./Leaderboard";
+
 function DetailsPage() {
-  return <div>Details</div>;
+  const { levelNum } = useParams();
+
+  return (
+    <>
+      <h1>level {levelNum}</h1>
+      <Link to={`/level/${levelNum}`}>Start</Link>
+      <Leaderboard levelNum={levelNum} />
+      <Link to={`/`}>Back</Link>
+    </>
+  );
 }
 
 export default DetailsPage;
