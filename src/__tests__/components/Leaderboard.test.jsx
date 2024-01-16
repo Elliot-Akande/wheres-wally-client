9b4e1db5c53184c84e2b31b37fd7b418492a65e7
Elliot Akande <elbo.lynch@gmail.com>
2024-01-16 14:08:55 +0000

    Create test file with render test on valid data

---
+++ src/__tests__/components/Leaderboard.test.jsx
@@ -0,0 +1,46 @@
+import { vi } from "vitest";
+import useLeaderboardData from "../../hooks/useLeaderboardData";
+import { render, screen } from "@testing-library/react";
+import Leaderboard from "../../components/Leaderboard";
+
+const mocks = vi.hoisted(() => {
+  return { useLeaderboardData: vi.fn() };
+});
+
+vi.mock("/src/hooks/useLeaderboardData.jsx", () => ({
+  default: mocks.useLeaderboardData,
+}));
+
+const levelNum = 3;
+const data = [
+  {
+    id: 3,
+    name: "Anon",
+    time: 32,
+  },
+  {
+    id: 1,
+    name: "Big Gary",
+    time: 100,
+  },
+  {
+    id: 2,
+    name: "Gordon",
+    time: 140,
+  },
+];
+const loading = false;
+const error = null;
+
+describe("Leaderboard", () => {
+  it("renders heading and list on valid leaderboard data", () => {
+    mocks.useLeaderboardData.mockReturnValue({ data, loading, error });
+
+    const { container } = render(<Leaderboard levelNum={levelNum} />);
+
+    expect(useLeaderboardData).toBe(mocks.useLeaderboardData);
+    expect(useLeaderboardData).toHaveBeenCalled();
+
+    expect(container).toMatchSnapshot();
+  });
+});
