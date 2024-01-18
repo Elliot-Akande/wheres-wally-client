76d654d774d36a7a2d3ea65819190717c1884a9d
Elliot Akande <elbo.lynch@gmail.com>
2024-01-18 11:38:48 +0000

    Uninstall eslint vitest globals

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,
     environment: "jsdom",
-    setupFiles: "./src/__tests__/setup.js",
+    setupFiles: "./vitestSetup.js",
   },
 });
