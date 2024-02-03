5d30192a6b985c19e8aa751337a18b5435baf3d5
Elliot Akande <elbo.lynch@gmail.com>
2024-02-03 21:54:22 +0000

    Add useFetch hook along with error handling

--- src/pages/LevelDetails/index.jsx
+++ src/pages/LevelDetails/index.jsx
@@ -1,24 +1,36 @@
 import { Link, useParams } from "react-router-dom";
 import Leaderboard from "../../components/Leaderboard/Leaderboard";
-import { useEffect } from "react";
-import checkLevelExists from "./checkLevelExists";
-import { useNavigate } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 const LevelDetails = () => {
   const { levelNum } = useParams();
-  const navigate = useNavigate();
+  const { data, loading, error } = useFetch(`/levels/${levelNum}/leaderboard`);
 
-  useEffect(() => {
-    if (!checkLevelExists(levelNum)) navigate("/");
-  }, []);
+  if (!error) {
+    return (
+      <>
+        <h1>level {levelNum}</h1>
+        <Link to={`/level/${levelNum}`}>Start</Link>
+        <Leaderboard data={data} loading={loading} />
+        <Link to={`/`}>Back</Link>
+      </>
+    );
+  }
+
+  if (error.status === 404) {
+    return (
+      <div>
+        <h2>This level doesn&apos;t exist!</h2>
+        <Link to="/">Back to home page</Link>
+      </div>
+    );
+  }
 
   return (
-    <>
-      <h1>level {levelNum}</h1>
-      <Link to={`/level/${levelNum}`}>Start</Link>
-      <Leaderboard levelNum={parseInt(levelNum, 10)} />
-      <Link to={`/`}>Back</Link>
-    </>
+    <div>
+      <h2>There was an error fetching level data!</h2>
+      <Link to="/">Back to home page</Link>
+    </div>
   );
 };
 
