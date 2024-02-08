8fbd154e090e84326fd7dd23ad1341eae6b6b74f
Elliot Akande <elbo.lynch@gmail.com>
2024-02-08 23:16:06 +0000

    Style header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <>
-      <h1>Where&apos;s Wally?</h1>
-      <Outlet />
-    </>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <Link to={"/"} className={styles.link}>
+          Where&apos;s Wally?
+        </Link>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
   );
 }
 
