fdb503c18e93a45f3feb2fc40b4a76debc371105
Elliot Akande <elbo.lynch@gmail.com>
2024-02-21 15:29:40 +0000

    Update Leaderboard tests

--- src/components/__tests__/Leaderboard.test.jsx
+++ src/components/__tests__/Leaderboard.test.jsx
@@ -1,88 +1,41 @@
-import { render } from "@testing-library/react";
-import { vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
 import Leaderboard from "../Leaderboard/Leaderboard";
-import useLeaderboardData from "../Leaderboard/useLeaderboardData";
-import { describe, it, expect } from "vitest";
 
-const mocks = vi.hoisted(() => {
-  return { useLeaderboardData: vi.fn() };
-});
-
-vi.mock("/src/components/Leaderboard/useLeaderboardData.jsx", () => ({
-  default: mocks.useLeaderboardData,
+vi.mock("../../utils/formatTime.js", () => ({
+  default: (time) => time,
 }));
 
-const levelNum = 3;
-const data = [
-  {
-    id: 3,
-    name: "Anon",
-    time: 32,
-  },
-  {
-    id: 1,
-    name: "Big Gary",
-    time: 100,
-  },
-  {
-    id: 2,
-    name: "Gordon",
-    time: 140,
-  },
-];
-const loading = false;
-const error = null;
+const data = {
+  scores: [
+    {
+      _id: "3",
+      name: "Anon",
+      score: 32,
+    },
+    {
+      _id: "1",
+      name: "Big Gary",
+      score: 100,
+    },
+  ],
+};
 
 describe("Leaderboard", () => {
   it("renders heading and list on valid leaderboard data", () => {
-    mocks.useLeaderboardData.mockReturnValue({ data, loading, error });
-
-    const { container } = render(<Leaderboard levelNum={levelNum} />);
-
-    expect(useLeaderboardData).toBe(mocks.useLeaderboardData);
-    expect(useLeaderboardData).toHaveBeenCalled();
-
+    const { container } = render(<Leaderboard data={data} loading={false} />);
     expect(container).toMatchSnapshot();
   });
 
   it("renders heading and loading message when loading data", () => {
-    mocks.useLeaderboardData.mockReturnValue({ data, loading: true, error });
-
-    const { container } = render(<Leaderboard levelNum={levelNum} />);
-
-    expect(useLeaderboardData).toBe(mocks.useLeaderboardData);
-    expect(useLeaderboardData).toHaveBeenCalled();
-
-    expect(container).toMatchSnapshot();
-  });
-
-  it("renders heading and error message on data fetch error", () => {
-    mocks.useLeaderboardData.mockReturnValue({
-      data,
-      loading,
-      error: new Error(),
-    });
-
-    const { container } = render(<Leaderboard levelNum={levelNum} />);
-
-    expect(useLeaderboardData).toBe(mocks.useLeaderboardData);
-    expect(useLeaderboardData).toHaveBeenCalled();
-
+    const { container } = render(<Leaderboard data={{}} loading={true} />);
     expect(container).toMatchSnapshot();
   });
 
   it("renders heading and empty scores message when no data after loading", () => {
-    mocks.useLeaderboardData.mockReturnValue({
-      data: null,
-      loading,
-      error,
-    });
-
-    const { container } = render(<Leaderboard levelNum={levelNum} />);
-
-    expect(useLeaderboardData).toBe(mocks.useLeaderboardData);
-    expect(useLeaderboardData).toHaveBeenCalled();
-
+    const { container } = render(
+      <Leaderboard data={{ scores: [] }} loading={false} />
+    );
     expect(container).toMatchSnapshot();
   });
 });
