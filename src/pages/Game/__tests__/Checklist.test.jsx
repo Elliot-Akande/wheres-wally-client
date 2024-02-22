65ae7527635aeb41ad679c9b12697ed22ebc8ff3
Elliot Akande <elbo.lynch@gmail.com>
2024-02-22 12:25:27 +0000

    Create Checklist component tests

---
+++ src/pages/Game/__tests__/Checklist.test.jsx
@@ -0,0 +1,27 @@
+import { render } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import Checklist from "../Checklist/Checklist.jsx";
+
+vi.mock("../ChecklistItem/ChecklistItem.jsx", () => ({
+  default: ({ key, character, found }) => (
+    <li key={key}>
+      {character.name} is {found ? "Found" : "Not Found"}
+    </li>
+  ),
+}));
+
+const characters = [
+  { name: "Harry", id: 1, imageUrl: "harry.jpg" },
+  { name: "Hermione", id: 2, imageUrl: "hermione.jpg" },
+];
+const correctAnswers = [{ character: "Hermione" }];
+
+describe("Checklist", () => {
+  it("renders a list of characters with correct found status", () => {
+    const { container } = render(
+      <Checklist characters={characters} correctAnswers={correctAnswers} />
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
