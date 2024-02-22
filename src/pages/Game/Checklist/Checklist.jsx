361ec585b602f33da7e1f1aca2fb0651e7e1b20c
Elliot Akande <elbo.lynch@gmail.com>
2024-02-22 12:05:33 +0000

    Add prop validation

--- src/pages/Game/Checklist/Checklist.jsx
+++ src/pages/Game/Checklist/Checklist.jsx
@@ -1,5 +1,6 @@
-import styles from "./Checklist.module.css";
+import PropTypes from "prop-types";
 import ChecklistItem from "../ChecklistItem/ChecklistItem";
+import styles from "./Checklist.module.css";
 
 const Checklist = ({ characters, correctAnswers }) => {
   const isFound = (character) => {
@@ -12,11 +13,25 @@ const Checklist = ({ characters, correctAnswers }) => {
         <ChecklistItem
           key={character.name}
           character={character}
-          found={isFound(character.name)}
+          found={!!isFound(character.name)}
         />
       ))}
     </ul>
   );
+};
+
+Checklist.propTypes = {
+  character: PropTypes.arrayOf([
+    PropTypes.shape({
+      name: PropTypes.string,
+      imageUrl: PropTypes.string,
+    }),
+  ]),
+  correctAnswers: PropTypes.arrayOf(
+    PropTypes.shape({
+      character: PropTypes.string,
+    })
+  ).isRequired,
 };
 
 export default Checklist;
